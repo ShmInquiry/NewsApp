ae16c6907c6fc2ea6b57a12a93dc6034a46283fb
ShmInquiry <Sh.m.Inquiry@outlook.com>
2024-03-19 11:34:07 +0300

    7th commit

--- NewsApp2/NewsApp2/APICaller.swift
+++ NewsApp2/NewsApp2/APICaller.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constats {
+        static let topHeadlinesURL  = URL (string:
+        "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=274099a79e6d4a679542c030ea58adc2")
+    }
+    
+    private init() {}
+
+    public func getTopStories(completion: @escaping (Result<[Article], Error>) -> Void)
+    {
+        guard let url = Constats.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) {data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    completion(.success(result.articles))
+                }
+                catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+}
+
+// Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable{
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
--- NewsApp2/NewsApp2/ViewController.swift
+++ NewsApp2/NewsApp2/ViewController.swift
@@ -2,18 +2,89 @@
 //  ViewController.swift
 //  NewsApp2
 //
-//  Created by NBK on 18/03/2024.
+//  Created by Sh.M on 18/03/2024.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+//Tableview
+// Custom Cell
+// Api caller
+// Open the news stories
+// Search for news stories
 
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(NewsTableViewCell.self, forCellReuseIdentifier: NewsTableViewCell.identifier)
+        
+        return table
+    }()
+    
+    private var viewModels = [NewsTableViewCellViewModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        title = "News"
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.backgroundColor = .systemBackground
+        
+        
+        APICaller.shared.getTopStories{ [weak self] result in
+            switch result {
+            case .success(let articles):
+                self?.viewModels = articles.compactMap({
+                    NewsTableViewCellViewModel(
+                        title: $0.title,
+                        subtitle: $0.description ?? "No Description",
+                        imageURL: URL(string: $0.urlToImage ?? "")
+                    )
+                })
+                
+                DispatchQueue.main.async {
+                    self?.tableView.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
-
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    
+    // Table views
+    func tableView(_ tableview: UITableView, numberOfRowsInSection section : Int) -> Int {
+        return viewModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: NewsTableViewCell.identifier,
+            for: indexPath
+        ) as? NewsTableViewCell else {
+                fatalError()
+            }
+        cell.configure(with: viewModels[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+    
 }
 
